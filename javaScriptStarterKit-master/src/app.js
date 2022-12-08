27f5757a85df08b955e704c82970ebd6670c97b9
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-12-09 01:43:36 +0300

    start

---
+++ javaScriptStarterKit-master/src/app.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+let dolarDun = 9.2;
+let dolarBugun = 10.3;
+
+console.log(dolarDun);
+const euroDun = 11.2;
+console.log(euroDun);
+
+let konutKredileri = ["Konut Kredisi", "Emlak Kredisi", "Bedelli Kredisi"];
+
+console.log("ul");
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log("<li>" + konutKredileri[i] + "</li>");
+}
+console.log("</ul>");
+
+console.log(konutKredileri);
